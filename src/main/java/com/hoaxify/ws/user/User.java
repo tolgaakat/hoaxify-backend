9016af55a8cae97ee4714e1388a0d3d805b211f3
TURKCELL\TCTAKAT <tolga.akat@turkcell.com.tr>
2022-08-02 10:37:20 +0300

    Section 3_31

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,24 +1,40 @@
 package com.hoaxify.ws.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name="USER")
+
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@NotNull
+	@Size(min=4, max=255)
+	//@Column(unique=true)
+	@UniqueUsername
 	private String username;
+	
+	@NotNull
+	@Size(min=4, max=255)
 	private String displayName;
+	
+	@NotNull
+	@Size(min=8, max=255)
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
 	private String password;
 	
 
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -3,14 +3,20 @@ package com.hoaxify.ws.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hoaxify.ws.error.ApiError;
@@ -25,31 +31,25 @@ public class UserController {
 	UserService userService;
 
 	@PostMapping("/api/1.0/users")
-	public ResponseEntity<?> createUser(@RequestBody User user) {
-		
-		ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
-		Map<String, String> validationErrors = new HashMap<>();
-		
-		String username = user.getUsername();
-		String displayName = user.getDisplayName();
-		
-		
-		if (username == null || username.isEmpty()) {		
-			validationErrors.put("username", "Username cannot be null");
-		}
-		
-		if (displayName == null || displayName.isEmpty()) {
-			validationErrors.put("displayName", "DisplayName cannot be null");
-		}
-		
-		if (validationErrors.size()>0){
-			error.setValidationErrors(validationErrors);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-		}
-		
+	public GenericResponse createUser(@Valid @RequestBody User user) {
+	
 		userService.save(user);
-		return ResponseEntity.ok(new GenericResponse("user created successfully!"));
+		return new GenericResponse("user created successfully!");
 
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiError handleValidationException(MethodArgumentNotValidException exception)
+	{
+		ApiError error= new ApiError( 400, "Validation error", "/api/1.0/users");
+		Map<String, String> validationErrors = new HashMap<>();
+		for (FieldError fieldError: exception.getBindingResult().getFieldErrors())
+		{
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		error.setValidationErrors(validationErrors);
+		return error;
 	}
 
 }
